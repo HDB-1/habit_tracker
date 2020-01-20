8f56dc1ac23b5784bb5d0c8b5f2e378c4493c5fe
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-01-20 14:59:57 +0000

    add function to get all users from user collection

---
+++ habit_tracker/scripts/queries.js
@@ -0,0 +1,18 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/';
+
+
+
+
+
+function Users() {
+    MongoClient.connect(url, function(err, db) {
+        if(err) throw err;
+        const dbo = db.db('habit_tracker');
+        let allUsers = dbo.collection('users').find({}).toArray(function(err, result) {
+            if (err) throw err;
+            db.close();
+        });
+        return allUsers;
+    }); 
+}
