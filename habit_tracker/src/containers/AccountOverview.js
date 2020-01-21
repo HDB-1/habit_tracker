8737bc235ec162a456a7e3204ad3de772942cbd8
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-01-21 15:45:10 +0000

    add api request for activities information. Currently in progress

--- habit_tracker/src/containers/AccountOverview.js
+++ habit_tracker/src/containers/AccountOverview.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export class accountOverview extends Component {
+    
+    getAllActivities(){
+        console.log(this.props.userInfo.username)
+    axios.get('http://localhost:4000/activities')
+    .then((res) => {
+        console.log(res.data)
+      this.setState({allActivities: res.data})
+    })
+    .catch((err) => {
+        console.log('erro in account overview' + err)
+      })
+  }
+componentDidMount() {
+    this.getAllActivities()
+}
+
     render() {
+        // console.log(this.props.userInfo)
         return (
             <div>
                 <h1>Welcome {this.props.userInfo.fn} {this.props.userInfo.ln}</h1>
+
             </div>
         )
     }
