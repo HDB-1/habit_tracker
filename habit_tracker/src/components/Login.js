ee7b4430bb6e98ddba28fd9c1c8a3fc01f7639ef
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-01-21 11:50:40 +0000

    add onChange handler for login boxes

--- habit_tracker/src/components/Login.js
+++ habit_tracker/src/components/Login.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Login() {
-    return (
-        <div>
-            <h3>Put your login info here</h3>
-            <form>
-                <label for="userName">Username
-                <input type="text" className = "userName" name="userName"></input>
-                </label>
-                <label for="password">Password
-                <input type="text" className = "password" name="password"></input>
-                </label>
-                <button type="submit" id="loginButton">Log in</button>
-            </form>
-        </div>
-    )
+export class Login extends Component {
+
+    handleChangeOfPassword = (event) => {
+        this.setState({currenInputPassword: event.target.value})
+        console.log(event.target.value)
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <h3>Put your login info here</h3>
+                <form>
+                    <label for="userName">Username
+                    <input type="text" className = "userName" name="userName"></input>
+                    </label>
+                    <label for="password">Password
+                    <input type="text" className = "password" name="password" onChange={(event) => this.handleChangeOfPassword(event)}></input>
+                    </label>
+                    <button type="submit" id="loginButton">Log in</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default Login
