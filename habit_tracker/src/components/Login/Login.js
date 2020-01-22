58ce063802f88b77ad24186ddc381fd769360af4
Dominic Holm-Kander <dominic.holmkander@gmail.com>
2020-01-22 19:10:01 +0000

    add tests equalling 100% coverage for Login component

--- habit_tracker/src/components/Login/Login.js
+++ habit_tracker/src/components/Login/Login.js
@@ -2,27 +2,25 @@ import React, { Component } from 'react'
 
 export class Login extends Component {
 
-    handleChangeOfPassword = (event) => {
-        this.setState({currenInputPassword: event.target.value})
+    handleChangeOfPassword = (input) => {
+        this.setState({currentInputPassword: input})
     }
-    handleChangeOfUsername = (event) => {
-        this.setState({currenInputUsername: event.target.value})
+    handleChangeOfUsername = (input) => {
+        this.setState({currentInputUsername: input})
     }
-
 
     render() {
-
 
         return (
             <div>
                 <h3>Put your login info here</h3>
                 <label for="userName">Username
-                <input type="text" className = "userName" name="userName" onChange={(event) => this.handleChangeOfUsername(event)}></input>
+                <input type="text" className = "userName" name="userName" onChange={(event) => this.handleChangeOfUsername(event.target.value)}></input>
                 </label>
                 <label for="password">Password
-                <input type="text" className = "password" name="password" onChange={(event) => this.handleChangeOfPassword(event)}></input>
+                <input type="text" className = "password" name="password" onChange={(event) => this.handleChangeOfPassword(event.target.value)}></input>
                 </label>
-                <button type="button" id="loginButton" onClick={() => this.props.checkCredentials(this.state.currenInputUsername, this.state.currenInputPassword)}>Log in</button>
+                <button type="button" id="loginButton" onClick={() => this.props.checkCredentials(this.state.currentInputUsername, this.state.currentInputPassword)}>Log in</button>
             </div>
         )
     }
--- habit_tracker/tests/Login.test.js
+++ habit_tracker/tests/Login.test.js
@@ -1,16 +1,55 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Login from "../src/components/Login/Login"
+import waitUntil from "async-wait-until"; // For async testing of setState functions.
+import regeneratorRuntime from 'regenerator-runtime';
 
 describe("shallow Login", () => {
     let wrapper;
+    let checkCredentialsMock = jest.fn()
 
-    beforeEach(() => (wrapper = shallow(<Login />)))
+    beforeEach(() => (wrapper = shallow(<Login checkCredentials={checkCredentialsMock}/>)))
     it("should render correctly", () => {
         expect(wrapper).toMatchSnapshot();
     })
     it("should have 2 input fields and a button", () => {
         expect(wrapper.find("input").length).toEqual(2);
         expect(wrapper.find("button").length).toEqual(1);
+    })
+    it("loginButton should call 'checkCredentials' function when clicked", 
+    async () => {
+        wrapper.setState({currentInputUsername: "placeholder", currentInputPassword: "alsoAPlaceholder"})
+        // Adding placeholder data is needed to stop the function throwing a TypeError when given parameters equal to null
+        await waitUntil(() => wrapper.instance().state.currentInputPassword && wrapper.instance().state.currentInputUsername);        expect(checkCredentialsMock).not.toHaveBeenCalled();
+        wrapper.find('button').simulate('click');
+        expect(checkCredentialsMock).toHaveBeenCalled();
+    })
+    it("HandleChangeOfPassword function should update component state", async () => {
+        wrapper.instance().handleChangeOfPassword('testPassword');
+        await waitUntil(() => wrapper.instance().state.currentInputPassword);
+        expect(wrapper.instance().state.currentInputPassword).toEqual('testPassword');
+    })
+    it("HandleChangeOfUsername function should update component state", async () => {
+        wrapper.instance().handleChangeOfUsername('testUsernameValue');
+        await waitUntil(() => wrapper.instance().state.currentInputUsername);
+        expect(wrapper.instance().state.currentInputUsername).toEqual('testUsernameValue');
+    })
+    it("Typing in the username input element should call handleChangeOfusername function", 
+    () => {
+        let spy = jest.spyOn(wrapper.instance(), "handleChangeOfUsername");
+        wrapper.instance().forceUpdate();
+        expect(spy).toHaveBeenCalledTimes(0);
+        wrapper.find('[name="userName"]').at(0)
+        .simulate("change", { target: { value: "anything"}});
+        expect(spy).toHaveBeenCalledTimes(1)
+    })
+    it("Typing in a password should call handleChangeOfPassword function", 
+    () => {
+        let spy = jest.spyOn(wrapper.instance(), "handleChangeOfPassword");
+        wrapper.instance().forceUpdate();  
+        expect(spy).toHaveBeenCalledTimes(0);
+        wrapper.find('[name="password"]').at(0)
+        .simulate("change", { target: { value: "whateverYouPlease"}});
+        expect(spy).toHaveBeenCalledTimes(1)
     })
 })
