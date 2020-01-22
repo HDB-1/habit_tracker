36aec40ed91e16166d8a4b8db12ea205bbc1fd15
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-01-22 18:25:24 +0000

    add tick and cross image as well as other styling feature for tasks

--- habit_tracker/src/components/Task/Task.js
+++ habit_tracker/src/components/Task/Task.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 import './Task.css'
+import tickImage from '../../pics/tick.png'
+import crossImage from '../../pics/cross.png'
 
 
 
@@ -8,15 +10,20 @@ export default function Task(props) {
     return (
         <div>
             <ul className={props.className}>
-                <li>This is the activity name {props.activity.activityName}</li>
-                <li>This is the frequency{props.activity.frequency}</li>
-                <li>{props.activity.completed ? "Task is complete" : "task outstanding"}</li>
-                <li>Priority flagger{props.activity.isPriority ? "This task is priority" : "this is not urgent"}</li>
-                <li>Last completed (horrible date format){props.activity.lastCompleted.toString().substring(0, 21)}</li>
-                <li>Streak{props.activity.streak}</li>
-                <button type = "button" onClick= {() => axios.post('http://localhost:4000/mark_task_as_complete', {activityName: props.activity.activityName})}>Mark task as complete</button>
+                <li className="activityName">{props.activity.activityName}</li>
+                <li>This is the frequency: {props.activity.frequency}</li>
+                {/* <li>{props.activity.completed ? "Task is complete" : "task outstanding"}</li> */}
+                <li>Priority flagger: {props.activity.isPriority ? "This task is priority" : "this is not urgent"}</li>
+                <li>Streak: {props.activity.streak}</li>
 
+                <button type = "button" onClick= {() => axios.post('http://localhost:4000/mark_task_as_complete', {activityName: props.activity.activityName})}>Mark task as complete</button>
+            <p className="lastCompleted">Last completed: {props.activity.lastCompleted.toString().substring(0, 21)}</p>
             </ul>
+                {props.activity.completed ? <img src={tickImage} className="tick" alt="tick"></img> : <img src={crossImage} className="tick" alt="tick"></img>}
+                
+                
         </div>
     )
 }
+
+
