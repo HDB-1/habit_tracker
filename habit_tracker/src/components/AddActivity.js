e576627d1a932ede8f6b4afc5ea8b898b215f654
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-01-22 11:27:16 +0000

    make add activity form

--- habit_tracker/src/components/AddActivity.js
+++ habit_tracker/src/components/AddActivity.js
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function AddActivity() {
-    return (
-        <div>
+export class AddActivity extends Component {
+
+    handleChangeOfActivityName = (event) => {
+        this.setState({activityName: event.target.value})
+    }
+    handleChangeOfFrequency = (event) => {
+        this.setState({frequency: event.target.value})
+    }
+    handleChangeOfPriority = (event) => {
+        this.setState({priority: event.target.value})
+    }
+    
+    render() {
+        return (
+            <div>
             <form>
                     <label for="activityName">Name of activity
-                    <input type="text" name="activityName"></input>
+                    <input type="text" name="activityName" 
+                    onChange = {(event) => this.handleChangeOfActivityName(event)}>
+                    </input>
                     </label>
                     <label for="frequency">Frequency
-                    <input type="text" name="frequency"></input>
+                    <select name="frequency" onChange = {(event) => this.handleChangeOfFrequency(event)}>
+                        <option value = "Daily">Daily</option>
+                        <option value = "Weekly">Weekly</option>
+                        <option value = "Monthly">Monthly</option>
+                    </select>
                     </label>
                     <label for="priority">Priority (probs actually a radio button)
-                    <input type="text" name="priority"></input>
+                    <input type="checkbox" name="priority" onChange = {(event) => this.handleChangeOfPriority(event)}/>This task is a priority
                     </label>
                     <button type="button" id="addActivityButton" >Add an activity! (jsx needs updating)</button>
                 </form>
         </div>
-    )
+        )
+    }
 }
+
+export default AddActivity
