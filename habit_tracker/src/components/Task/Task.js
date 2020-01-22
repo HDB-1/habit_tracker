0dbe8732ffe5604844899ce51efaaeb14c92a32e
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-01-22 16:40:38 +0000

    add button to mark tasks as complete via route "mark_task_is_complete"

--- habit_tracker/src/components/Task/Task.js
+++ habit_tracker/src/components/Task/Task.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 export default function Task(props) {
     return (
@@ -10,6 +11,7 @@ export default function Task(props) {
                 <li>{props.activity.isPriority ? "This task is priority" : "this is not urgent"}</li>
                 <li>{props.activity.lastCompleted}</li>
                 <li>{props.activity.streak}</li>
+                <button type = "button" onClick= {() => axios.post('http://localhost:4000/mark_task_as_complete', {activityName: props.activity.activityName})}>Mark task as complete</button>
                 <li>----------------------</li>
             </ul>
         </div>
