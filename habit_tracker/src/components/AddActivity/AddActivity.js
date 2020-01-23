fcd85e4cd5d1eac58e5497b684edfae2f5369dc1
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-01-23 16:22:50 +0000

    make signup form and add activity form

--- habit_tracker/src/components/AddActivity/AddActivity.js
+++ habit_tracker/src/components/AddActivity/AddActivity.js
@@ -25,23 +25,26 @@ export class AddActivity extends Component {
         return (
             <div>
             <form>
-                <label for="activityName">Name of activity
-                <input type="text" name="activityName" 
-                onChange = {(event) => this.handleChangeOfActivityName(event)}>
-                </input>
-                </label>
-                <label for="frequency">Frequency
-                <select name="frequency" onChange = {(event) => this.handleChangeOfFrequency(event)}>
-                    <option value = "Daily">Daily</option>
-                    <option value = "Weekly">Weekly</option>
-                    <option value = "Monthly">Monthly</option>
-                </select>
-                </label>
-                <label for="priority">Priority (probs actually a radio button)
-                <input type="checkbox" name="priority" onClick = {() => this.togglePriority()}/>This task is a priority
-                </label>
-                <button type="button" id="addActivityButton" onClick={() => this.props.addNewActivity(this.state)}>Add an activity! (jsx needs updating)</button>
-            </form>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">What do you need to do?</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="2"  onChange = {(event) => this.handleChangeOfActivityName(event)}></textarea>
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlSelect1">Frequency</label>
+    <select class="form-control" id="exampleFormControlSelect1" onChange = {(event) => this.handleChangeOfFrequency(event)}>
+    <option value = "Daily">Daily</option>
+    <option value = "Weekly">Weekly</option>
+    <option value = "Monthly">Monthly</option>
+    </select>
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1" name="priority" onClick = {() => this.togglePriority()}></input>
+    <label class="form-check-label" for="exampleCheck1">Is this habit important?</label>
+  </div>
+  <button type="button" class="btn btn-primary" id="addActivityButton" onClick={() => this.props.addNewActivity(this.state)}>Add</button>
+
+</form>
         </div>
         )
     }
--- habit_tracker/src/components/Signup/Signup.js
+++ habit_tracker/src/components/Signup/Signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import './Signup.css'
 export class Signup extends Component {
 
     handleChangeOfUsername = (event) => {
@@ -19,44 +19,38 @@ export class Signup extends Component {
     render() {
         return (
           <div>
-            <h3>Or if you're a first-time visitor, sign up below!</h3>
+            <h4>If you're a first-time visitor, sign up below!</h4>
+    <div className="signup">
             <form clasName="needs-validation" novalidate>
               <div clasName="form-row">
                 <div clasName="col-md-4 mb-3">
-                  <label for="validationCustom01">First name</label>
+                  {/* <label for="validationCustom01">First name</label> */}
                   <input type="text" clasName="form-control" id="validationCustom01" placeholder="First name" name="fn" required onChange={(event) => this.handleChangeOfFn(event)}></input>
-                  <div clasName="valid-feedback">
-                    Looks good!
-                  </div>
+                  
                 </div>
               </div>
               <div clasName="col-md-4 mb-3">
-                <label for="validationCustom02">Last name</label>
+                {/* <label for="validationCustom02">Last name</label> */}
                 <input type="text" clasName="form-control" id="validationCustom02" placeholder="Last name" name="ln" onChange={(event) => this.handleChangeOfLn(event)} required></input>
-                <div clasName="valid-feedback">
-                  Looks good!
                 </div>
-              </div>
+              
               <div clasName="col-md-4 mb-3">
-                <label for="validationCustomUsername">Username</label>
-                <div clasName="input-group">
-                  <div clasName="input-group-prepend">
-                    <span clasName="input-group-text" id="inputGroupPrepend">@</span>
-                  </div>
-                  <input type="text" clasName="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" name="userName" onChange={(event) => this.handleChangeOfUsername(event)} required></input>
-                  <div clasName="invalid-feedback">
-                    Please choose a username.
-                  </div>
+                {/* <label for="validationCustom02">Username</label> */}
+                <input type="text" clasName="form-control" id="validationCustom02" placeholder="Username" name="userName" onChange={(event) => this.handleChangeOfUsername(event)} required></input>
                 </div>
-              </div>
+              <div clasName="col-md-4 mb-3">
+                {/* <label for="validationCustom02">Password</label> */}
+                <input type="password" clasName="form-control" id="validationCustom03" placeholder="Password" name="password" onChange={(event) => this.handleChangeOfPassword(event)} required></input>
+                </div>
+              
+             
               <div clasName="form-row">
                 <div clasName="col-md-6 mb-3">
-                  <label for="validationCustom03">Password</label>
-                  <input type="password" clasName="form-control" id="validationCustom03" placeholder="Password" name="password" onChange={(event) => this.handleChangeOfPassword(event)} required></input>
-                  <button className="btn btn-primary" type="submit" onClick={() => this.props.signup(this.state)}>Submit form</button>
+                  <button className="btn btn-primary" type="submit" onClick={() => this.props.signup(this.state)}>Sign up</button>
                 </div>
               </div>
             </form>
+            </div>
           </div>
         )
     }
