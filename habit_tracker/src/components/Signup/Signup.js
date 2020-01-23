0881e23338d11f1d65bef43fe39c7e3399614a73
jadesuzarte <57177552+jadesuzarte@users.noreply.github.com>
2020-01-23 15:26:05 +0000

    add functions to sign up form

--- habit_tracker/src/components/Signup/Signup.js
+++ habit_tracker/src/components/Signup/Signup.js
@@ -24,7 +24,7 @@ export class Signup extends Component {
               <div clasName="form-row">
                 <div clasName="col-md-4 mb-3">
                   <label for="validationCustom01">First name</label>
-                  <input type="text" clasName="form-control" id="validationCustom01" placeholder="First name" name="fn" required></input>
+                  <input type="text" clasName="form-control" id="validationCustom01" placeholder="First name" name="fn" required onChange={(event) => this.handleChangeOfFn(event)}></input>
                   <div clasName="valid-feedback">
                     Looks good!
                   </div>
@@ -32,7 +32,7 @@ export class Signup extends Component {
               </div>
               <div clasName="col-md-4 mb-3">
                 <label for="validationCustom02">Last name</label>
-                <input type="text" clasName="form-control" id="validationCustom02" placeholder="Last name" name="ln" required></input>
+                <input type="text" clasName="form-control" id="validationCustom02" placeholder="Last name" name="ln" onChange={(event) => this.handleChangeOfLn(event)} required></input>
                 <div clasName="valid-feedback">
                   Looks good!
                 </div>
@@ -43,7 +43,7 @@ export class Signup extends Component {
                   <div clasName="input-group-prepend">
                     <span clasName="input-group-text" id="inputGroupPrepend">@</span>
                   </div>
-                  <input type="text" clasName="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" name="userName" required></input>
+                  <input type="text" clasName="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" name="userName" onChange={(event) => this.handleChangeOfUsername(event)} required></input>
                   <div clasName="invalid-feedback">
                     Please choose a username.
                   </div>
@@ -52,8 +52,8 @@ export class Signup extends Component {
               <div clasName="form-row">
                 <div clasName="col-md-6 mb-3">
                   <label for="validationCustom03">Password</label>
-                  <input type="password" clasName="form-control" id="validationCustom03" placeholder="Password" name="password" required></input>
-                  <button className="btn btn-primary" type="submit">Submit form</button>
+                  <input type="password" clasName="form-control" id="validationCustom03" placeholder="Password" name="password" onChange={(event) => this.handleChangeOfPassword(event)} required></input>
+                  <button className="btn btn-primary" type="submit" onClick={() => this.props.signup(this.state)}>Submit form</button>
                 </div>
               </div>
             </form>
