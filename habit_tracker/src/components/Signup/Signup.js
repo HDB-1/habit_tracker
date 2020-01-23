d780d36747e0854cb704698879f320320141448a
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-01-23 15:12:39 +0000

    format signup and correct divs, and remove value in first name and last name for signup

--- habit_tracker/src/components/Signup/Signup.js
+++ habit_tracker/src/components/Signup/Signup.js
@@ -18,47 +18,46 @@ export class Signup extends Component {
 
     render() {
         return (
-            <div>
-                <h3>Or if you're a first-time visitor, sign up below!</h3>
-                <form clasName="needs-validation" novalidate>
-  <div clasName="form-row">
-    <div clasName="col-md-4 mb-3">
-      <label for="validationCustom01">First name</label>
-      <input type="text" clasName="form-control" id="validationCustom01" placeholder="First name" value="Mark" name="fn" required></input>
-      <div clasName="valid-feedback">
-        Looks good!
-      </div>
-    </div>
-    <div clasName="col-md-4 mb-3">
-      <label for="validationCustom02">Last name</label>
-      <input type="text" clasName="form-control" id="validationCustom02" placeholder="Last name" name="ln" value="Otto" required></input>
-      <div clasName="valid-feedback">
-        Looks good!
-      </div>
-    </div>
-    <div clasName="col-md-4 mb-3">
-      <label for="validationCustomUsername">Username</label>
-      <div clasName="input-group">
-        <div clasName="input-group-prepend">
-          <span clasName="input-group-text" id="inputGroupPrepend">@</span>
-        </div>
-        <input type="text" clasName="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" name="userName" required></input>
-        <div clasName="invalid-feedback">
-          Please choose a username.
-        </div>
-      </div>
-    </div>
-  </div>
-  <div clasName="form-row">
-    <div clasName="col-md-6 mb-3">
-      <label for="validationCustom03">Password</label>
-      <input type="password" clasName="form-control" id="validationCustom03" placeholder="Password" name="password" required>
-      
-  <button className="btn btn-primary" type="submit">Submit form</button>
-</form>
-
-
-            </div>
+          <div>
+            <h3>Or if you're a first-time visitor, sign up below!</h3>
+            <form clasName="needs-validation" novalidate>
+              <div clasName="form-row">
+                <div clasName="col-md-4 mb-3">
+                  <label for="validationCustom01">First name</label>
+                  <input type="text" clasName="form-control" id="validationCustom01" placeholder="First name" name="fn" required></input>
+                  <div clasName="valid-feedback">
+                    Looks good!
+                  </div>
+                </div>
+              </div>
+              <div clasName="col-md-4 mb-3">
+                <label for="validationCustom02">Last name</label>
+                <input type="text" clasName="form-control" id="validationCustom02" placeholder="Last name" name="ln" required></input>
+                <div clasName="valid-feedback">
+                  Looks good!
+                </div>
+              </div>
+              <div clasName="col-md-4 mb-3">
+                <label for="validationCustomUsername">Username</label>
+                <div clasName="input-group">
+                  <div clasName="input-group-prepend">
+                    <span clasName="input-group-text" id="inputGroupPrepend">@</span>
+                  </div>
+                  <input type="text" clasName="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" name="userName" required></input>
+                  <div clasName="invalid-feedback">
+                    Please choose a username.
+                  </div>
+                </div>
+              </div>
+              <div clasName="form-row">
+                <div clasName="col-md-6 mb-3">
+                  <label for="validationCustom03">Password</label>
+                  <input type="password" clasName="form-control" id="validationCustom03" placeholder="Password" name="password" required></input>
+                  <button className="btn btn-primary" type="submit">Submit form</button>
+                </div>
+              </div>
+            </form>
+          </div>
         )
     }
 }
